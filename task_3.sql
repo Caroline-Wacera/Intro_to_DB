f8bf7bd85071aee6b8c77453f7f779483fa67691
Caroline-Wacera <carolinewacera4@gmail.com>
2025-01-30 14:43:40 +0300

    making corrections on the script

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,7 @@
--- Select all tables in the alx_book_store database
+-- Select the database
+USE alx_book_store;
+
+-- List all tables in alx_book_store
 SELECT table_name 
 FROM INFORMATION_SCHEMA.TABLES 
-WHERE table_schema = DATABASE();
+WHERE table_schema = 'alx_book_store';
