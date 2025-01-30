528376111c9fec4f3912a73cf23be75c4d1523e3
Caroline-Wacera <carolinewacera4@gmail.com>
2025-01-30 14:46:46 +0300

    added the show tables required

--- task_3.sql
+++ task_3.sql
@@ -1,7 +1,5 @@
 -- Select the database
 USE alx_book_store;
 
--- List all tables in alx_book_store
-SELECT table_name 
-FROM INFORMATION_SCHEMA.TABLES 
-WHERE table_schema = 'alx_book_store';
+-- Show all tables in the database
+SHOW TABLES;
