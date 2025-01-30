7cbbbfb78fc9718b0abae0690c2a6d1904e6adcf
Caroline-Wacera <carolinewacera4@gmail.com>
2025-01-30 15:01:17 +0300

    correcting the script

--- task_4.sql
+++ task_4.sql
@@ -4,4 +4,5 @@ USE alx_book_store;
 -- Retrieve column information for the 'books' table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+AND TABLE_NAME = 'Books';  -- Ensure this is uppercase as per your requirement
