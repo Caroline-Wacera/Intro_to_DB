547b12f29dec191f10a3cf26147b262df1546dae
Caroline-Wacera <carolinewacera4@gmail.com>
2025-01-30 14:55:09 +0300

    going back to the previous script

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
 -- Select the database
 USE alx_book_store;
 
--- Retrieve table structure without using DESCRIBE, EXPLAIN, or ANALYZE
+-- Retrieve the full structure of the books table without DESCRIBE, EXPLAIN, or ANALYZE
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA 
 FROM INFORMATION_SCHEMA.COLUMNS 
 WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
