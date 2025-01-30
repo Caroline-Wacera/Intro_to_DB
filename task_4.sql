ec4535b61826b0fd7f054dac63d8f42498d416f1
Caroline-Wacera <carolinewacera4@gmail.com>
2025-01-30 14:50:11 +0300

    Fixed task_4.sql to retrieve table structure without DESCRIBE or EXPLAIN

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+-- Select the database
+USE alx_book_store;
+
+-- Retrieve the full description of the books table without using DESCRIBE or EXPLAIN
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
